20695dbd10a0200a007e0d55a318a12bbb0e05d4
helluy <helluy@unistra.fr>
2021-09-15 17:23:05 +0200

    up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+fn print_coo(val: Vec<f64>, iv: Vec<usize>, jv: Vec<usize>) {
+    // first search the size of the matrix
+    let imax = iv.iter().max().unwrap() + 1;
+    println!("nrows={}", imax);
+    let jmax = jv.iter().max().unwrap() + 1;
+    println!("ncols={}", jmax);
+
+    let mut full = vec![0.; imax * jmax];
+
+    let nz = val.len();
+
+    assert_eq!(nz, iv.len());
+    assert_eq!(nz, jv.len());
+
+    for (v, (i, j)) in val.iter().zip(iv.iter().zip(jv.iter())) {
+        full[i * imax + j] = *v;
+    }
+
+    println!("full=");
+    for i in 0..imax {
+        for j in 0..jmax {
+            print!("{} ", full[i * jmax + j]);
+        }
+        println!("");
+    }
+    println!("full={:?}", full);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let val = vec![2., 2., 2.];
+
+    let iv = vec![0, 1, 2];
+    let jv = vec![0, 1, 2];
+
+    print_coo(val, iv, jv);
 }
