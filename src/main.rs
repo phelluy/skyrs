525f2a98989dc0bb9508fac25541289359b775a0
helluy <helluy@unistra.fr>
2021-12-01 20:22:03 +0100

    up

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn print_coo(val: Vec<f64>, iv: Vec<usize>, jv: Vec<usize>) {
         }
         println!("");
     }
-    println!("full={:?}", full);
+    //println!("full={:?}", full);
 }
 
 /// Convert a coo matrix to compressed sparse row (csr) format
@@ -32,17 +32,25 @@ fn coo_to_csr(
     val_coo: Vec<f64>,
     iv: Vec<usize>,
     jv: Vec<usize>,
-    val_csr: &mut Vec<f64>,
-    col_index: &mut Vec<usize>,
-    row_start: &mut Vec<usize>,
-) {
+) -> (Vec<f64>,
+    Vec<usize>,
+    Vec<usize>) 
+{
     // some checks
     assert_eq!(val_coo.len(), iv.len());
     assert_eq!(val_coo.len(), jv.len());
-    let nrows = *iv.iter().max().unwrap();
+    let nrows = *iv.iter().max().unwrap() + 1;
     // first count non zero values in each row
-    let mut row_count = vec![0, nrows];
+    let mut row_count = vec![0; nrows];
     iv.iter().for_each(|iv| row_count[*iv] += 1);
+    println!("{:?}",row_count);
+    let mut row_start = vec![0; nrows+1];
+    for i in 0..nrows {
+        row_start[i+1] = row_start[i] + row_count[i]; 
+    }
+    let nnz = row_start[nrows];
+    println!("{:?}",row_start);
+    (val_coo, iv, jv)
 }
 
 // the following line define a triplet structure with
@@ -76,8 +84,18 @@ impl PartialEq for Triplet {
 
 impl Eq for Triplet {}
 
-fn coo_sort_compress(val: Vec<f64>, iv: Vec<usize>, jv: Vec<usize>) 
--> (Vec<f64>, Vec<usize>, Vec<usize>) {
+fn coo_sort_compress(
+    val: Vec<f64>,
+    iv: Vec<usize>,
+    jv: Vec<usize>,
+) -> (Vec<f64>, Vec<usize>, Vec<usize>) {
+
+    // initial verifications
+    assert_eq!(val.len(), iv.len());
+    assert_eq!(val.len(), jv.len());
+    if val.len() == 0 {
+        return (val ,iv, jv)
+    }
     // copy the vector in a vector of tuples
     let mut vtuple: Vec<Triplet> = iv
         .iter()
@@ -90,39 +108,57 @@ fn coo_sort_compress(val: Vec<f64>, iv: Vec<usize>, jv: Vec<usize>)
         })
         .collect();
     vtuple.sort();
-    println!("{:?}", vtuple);
-    let mut iv:Vec<usize>=vec![];
-    let mut jv: Vec<usize>=vec![];
-    let mut val: Vec<f64>=vec![];
+    //println!("{:?}", vtuple);
+    let mut iv: Vec<usize> = vec![];
+    let mut jv: Vec<usize> = vec![];
+    let mut val: Vec<f64> = vec![];
+
     let mut tr1 = vtuple[0];
     iv.push(tr1.i);
     jv.push(tr1.j);
     let mut cval = tr1.v;
 
-    vtuple.iter().skip(1).for_each(|tr2| if *tr2 == tr1 {
-        cval += tr2.v;
-    } else {
-        val.push(cval);
-        tr1 = *tr2;
-        iv.push(tr1.i);
-        jv.push(tr1.j);
-        cval = tr1.v;  
+    vtuple.iter().skip(1).for_each(|tr2| {
+        if *tr2 == tr1 {
+            cval += tr2.v;
+        } else {
+            val.push(cval);
+            tr1 = *tr2;
+            iv.push(tr1.i);
+            jv.push(tr1.j);
+            cval = tr1.v;
+        }
     });
     // last push
     val.push(cval);
-    // let iv: Vec<usize> = vtuple.iter().map(|trip| trip.i).collect();
-    // let jv: Vec<usize> = vtuple.iter().map(|trip| trip.j).collect();
-    // let val: Vec<f64> = vtuple.iter().map(|trip| trip.v).collect();
-    println!("{:?}",val);
-    (val,iv,jv)
+
+    (val, iv, jv)
 }
 
 fn main() {
-    let val = vec![2., 2., 1., 1.];
 
-    let iv = vec![2, 1, 0, 0];
-    let jv = vec![2, 1, 0, 0];
+    let mut val = vec![];
 
-    let (val,iv,jv) = coo_sort_compress(val, iv, jv);
+    let mut iv = vec![];
+    let mut jv = vec![];
+
+    let n = 5;
+
+    for i in 0..n {
+        val.push(2.);
+        iv.push(i);
+        jv.push(i);
+    }
+    for i in 0..n-1 {
+        val.push(-1.);
+        iv.push(i);
+        jv.push(i+1);
+        val.push(-1.);
+        iv.push(i+1);
+        jv.push(i);
+    }
+
+    let (val, iv, jv) = coo_sort_compress(val, iv, jv);
+    let (val, iv, jv) = coo_to_csr(val, iv, jv);
     print_coo(val, iv, jv);
 }
