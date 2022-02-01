d145f4a612e049f8dc8a028d2efb1aac789afa77
ph <helluy@unistra.fr>
2022-02-01 18:37:50 +0100

    modify the plot function in order to create locally the python command file

--- src/lib.rs
+++ src/lib.rs
@@ -1322,13 +1322,65 @@ pub fn doolittle_lu(a: &mut Vec<Vec<f64>>) {
     }
 }
 
+fn writepycom() {
+    // use std::path::Path;
+    // if 
+    let pycom = r#"
+from matplotlib.pyplot import *
+from math import *
+import numpy as np
+
+with open("skyrs_plotpy.dat", "r") as f:
+    contenu = f.read().split("\n\n")
+    # print(contenu)
+
+
+x = contenu[0].split()
+nx = len(x) - 1
+x = np.array([float(x[i]) for i in range(nx+1)])
+# print(x)
+y = contenu[1].split()
+ny = len(y) -1
+y = np.array([float(y[i]) for i in range(ny+1)])
+# print(y)
+x , y = np.meshgrid(x,y)
+z = contenu[2].split()
+nz = len(z)
+z = np.array([float(z[i]) for i in range(nz)]).reshape((ny+1,nx+1))
+# print(z)
+# plot(xi, unum, color="blue")
+# plot(xi, uex, color="red")
+# def quit_figure(event):
+#     if event.key == 'q':
+#         close(event.canvas.figure)
+# cid = gcf().canvas.mpl_connect('key_press_event', quit_figure)
+
+
+fig, ax = subplots()
+ax.axis('equal')
+cs = ax.pcolormesh(x,y,z,cmap = 'jet')
+# cs = ax.countourf(x,y,z,100,cmap = 'jet')
+cbar = fig.colorbar(cs)
+
+print("press \'q\' to quit...");
+show()
+"#;
+    use std::fs::File;
+    use std::io::BufWriter;
+    use std::io::Write;
+
+    let meshfile = File::create("skyrs_plot.py").unwrap();
+    let mut meshfile = BufWriter::new(meshfile); // create a buffer for faster writes...
+    writeln!(meshfile, "{}", pycom).unwrap();
+}
 /// Plots a 2D data set using matplotlib.
 fn plotpy(xp: Vec<f64>, yp: Vec<f64>, zp: Vec<f64>) {
     use std::fs::File;
     use std::io::BufWriter;
     use std::io::Write;
     {
-        let meshfile = File::create("plotpy.dat").unwrap();
+        writepycom();
+        let meshfile = File::create("skyrs_plotpy.dat").unwrap();
         let mut meshfile = BufWriter::new(meshfile); // create a buffer for faster writes...
         xp.iter().for_each(|x| {
             writeln!(meshfile, "{}", x).unwrap();
@@ -1345,7 +1397,7 @@ fn plotpy(xp: Vec<f64>, yp: Vec<f64>, zp: Vec<f64>) {
 
     use std::process::Command;
     Command::new("python3")
-        .arg("examples/plot.py")
+        .arg("skyrs_plot.py")
         .status()
         .expect("Plot failed: you need Python3 and Matplotlib in your PATH.");
 }
